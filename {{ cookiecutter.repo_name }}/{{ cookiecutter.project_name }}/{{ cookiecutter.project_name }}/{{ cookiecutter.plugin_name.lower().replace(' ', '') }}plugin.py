54eee26fff13f94eb6380b092d3bba2a78158341
Rafael Laverde <leafar91@gmail.com>
2017-01-16 17:32:40 -0500

    Add template for plugin file.

---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/{{ cookiecutter.plugin_name.lower().replace(' ', '') }}plugin.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © {{ cookiecutter.author }}
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""{{ cookiecutter.plugin_name }} Plugin."""
+
+from spyder.api.plugins import SpyderPluginWidget
+{% if cookiecutter.create_config_page == 'y' %}
+from spyder.api.preferences import PluginConfigPage
+{% endif %}
+
+{% if cookiecutter.create_config_page == 'y' %}
+class {{ cookiecutter.plugin_name.replace(' ', '') }}ConfigPage(PluginConfigPage):
+    """{{ cookiecutter.plugin_name }} plugin preferences."""
+    pass
+{% endif %}
+
+class {{ cookiecutter.plugin_name.replace(' ', '') }}Plugin(SpyderPluginWidget):
+    """{{ cookiecutter.plugin_name }} plugin."""
+    pass
+
