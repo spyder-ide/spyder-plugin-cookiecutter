983525dcc28c85378e8772d6ad7f9c794cdeac63
Rafael Laverde <leafar91@gmail.com>
2017-01-16 16:11:53 -0500

    Add template for plugin widget

---
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.project_name }}/{{ cookiecutter.project_name }}/widgets/{{ cookiecutter.plugin_name.lower().replace(' ', '') }}gui.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © {{ cookiecutter.author }}
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""{{ cookiecutter.plugin_name }} Widget."""
+
+from qtpy.QtWidgets import QWidget
+
+class {{ cookiecutter.plugin_name.replace(' ', '') }}Widget(QWidget):
+    """{{ cookiecutter.plugin_name.replace(' ', '') }} widget"""
+    pass
