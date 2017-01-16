38c241b5fa156c6375b64b1020f948d6bb0c8309
Rafael Laverde <leafar91@gmail.com>
2017-01-16 15:31:08 -0500

    Change copyright, use {{cookiecutter.author}}

---
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright © {{ cookiecutter.author }}
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Setup script for {{ cookiecutter.project_name }}."""
